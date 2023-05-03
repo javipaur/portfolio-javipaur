0373832001d1a33e4a664f50daec0917ffdd499d
Javier Palacio <javipaur@gmail.com>
2023-05-03 12:32:37 +0200

    first commit

---
+++ src/projects.mjs
@@ -0,0 +1,28 @@
+/* eslint-disable quotes */
+export const projects = [
+  {
+    name: "Davidlanaspa.com",
+    color: "color-primary",
+    description: "Web corporativa para despacho de abogados ",
+    techs: ["wordpress"],
+    image: "images/wuptick.png",
+    url: "https://github.com/samarjaffal/wuptick-frontend",
+  },
+  {
+    name: "Gasteizclick",
+    color: "color-secondary",
+    description: "Agenda digital para la ciudad de Vitoria Gasteiz",
+    techs: ["javascript", "react", "node", "mongodb"],
+    image: "images/wunderhod.png",
+    url: "https://github.com/samarjaffal/services-app",
+  },
+  {
+    name: "HelloChain",
+    color: "color-tertiary",
+    description:
+      'A web application for testing purposes when a user can sign in using Metamask and leave a "hello message" through the blockchain.',
+    techs: ["javascript", "react", "tailwindcss", "solidity", "hardhat"],
+    image: "images/hellochain.png",
+    url: "https://hellochain-app.samarjaffal.repl.co/",
+  },
+];
