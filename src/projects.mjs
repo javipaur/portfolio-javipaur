570afb2612c36d36b277b60eb5a8d7176dc79b7b
Javier Palacio <javipaur@gmail.com>
2023-05-03 12:50:17 +0200

    config

--- src/projects.mjs
+++ src/projects.mjs
@@ -4,9 +4,9 @@ export const projects = [
     name: "Davidlanaspa.com",
     color: "color-primary",
     description: "Web corporativa para despacho de abogados ",
-    techs: ["wordpress"],
+    techs: ["wordpress", "elementor"],
     image: "images/wuptick.png",
-    url: "https://github.com/samarjaffal/wuptick-frontend",
+    url: "https://github.com/samarjaffal/wuptick-frontend"
   },
   {
     name: "Gasteizclick",
@@ -14,15 +14,6 @@ export const projects = [
     description: "Agenda digital para la ciudad de Vitoria Gasteiz",
     techs: ["javascript", "react", "node", "mongodb"],
     image: "images/wunderhod.png",
-    url: "https://github.com/samarjaffal/services-app",
-  },
-  {
-    name: "HelloChain",
-    color: "color-tertiary",
-    description:
-      'A web application for testing purposes when a user can sign in using Metamask and leave a "hello message" through the blockchain.',
-    techs: ["javascript", "react", "tailwindcss", "solidity", "hardhat"],
-    image: "images/hellochain.png",
-    url: "https://hellochain-app.samarjaffal.repl.co/",
-  },
-];
+    url: "https://github.com/samarjaffal/services-app"
+  }
+]
